4f1d2e46235beb0c69e16e9432f144ee27ca9814
Salehz <saleh.zarei@gmail.com>
2019-05-29 18:51:43 +0430

    add buy ticket

---
+++ lib/buyticket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuyTicket extends StatefulWidget {
+  @override
+  _BuyTicketState createState() => _BuyTicketState();
+}
+
+class _BuyTicketState extends State<BuyTicket> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
