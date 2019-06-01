cd2921be8c189454b37d96be9964b7f117615b03
Salehz <saleh.zarei@gmail.com>
2019-06-01 18:01:49 +0430

    تغییر جدید

--- lib/UI/passenger.dart
+++ lib/UI/passenger.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jalali_calendar/jalali_calendar.dart';
 
 class Passenger extends StatefulWidget {
   final List personcount;
@@ -10,16 +11,155 @@ class Passenger extends StatefulWidget {
 }
 
 class _PassengerState extends State<Passenger> {
-  
   @override
   Widget build(BuildContext context) {
     print(widget.personcount);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.personcount.toString()),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.personcount.toString()),
+        ),
+        body: Directionality(
+            textDirection: TextDirection.rtl,
+            child: ListView(children: <Widget>[UserForm()])));
+  }
+}
+
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  int _radioValue = 0;
+  String _value ='انتخاب تاریخ';
+
+  void _changeValue(int value) {
+    setState(() {
+      _radioValue = value;
+    });
+  }
+
+  Future _selectDate() async {
+    String picked = await jalaliCalendarPicker(
+        context: context); // نمایش خروجی به صورت شمسی
+    //  await jalaliCalendarPicker(context: context,convertToGregorian: true); // نمایش خروجی به صورت میلادی
+    if (picked != null) setState(() => _value = picked.toString());
+   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        textDirection: TextDirection.rtl,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: 'نام', counterText: ''),
+                  keyboardType: TextInputType.text,
+                  maxLength: 10,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                flex: 1,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'نام خانوادگی',
+                    counterText: '',
+                  ),
+                  keyboardType: TextInputType.text,
+                  maxLength: 25,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: 'کدملی/پاسپورت', counterText: ''),
+                  keyboardType: TextInputType.text,
+                  maxLength: 10,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                flex: 1,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'تاریخ تولد',
+                    counterText: '',
+                  ),
+                  keyboardType: TextInputType.text,
+                  maxLength: 25,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    alignment: WrapAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'زن',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      Radio(
+                        value: 0,
+                        groupValue: _radioValue,
+                        onChanged: _changeValue,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'مرد',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      Radio(
+                        value: 1,
+                        groupValue: _radioValue,
+                        onChanged: _changeValue,
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                  flex: 1,
+                  child: FlatButton.icon(
+                    icon: Icon(Icons.calendar_today),
+                    label: Text(_value),
+                   onPressed: () {
+                   //  DatePicker.showDatePicker(context);
+                     _selectDate();
+                    },
+                  )
+                  
+                  )
+            ],
+          )
+        ],
       ),
-     
     );
   }
 }
