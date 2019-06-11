fe9e984c7ccd26d4c10fbc73df2fe49c50f271fd
navid13 <navidnejati76@gmail.com>
2019-06-12 00:57:00 +0430

    aboutus-1

--- lib/pages/aboutus.dart
+++ lib/pages/aboutus.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'درباره ما',
+      home: Container(
+        child: Image.asset('about-1.jpg'),
+      ),
     );
   }
 }
