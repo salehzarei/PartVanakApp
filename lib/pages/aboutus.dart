010051fc8bd5f1dd135f467c6d394535d41fcf3d
Salehz <saleh.zarei@gmail.com>
2019-06-12 18:40:48 +0430

    تغییرات در صفحه درباره ما

--- lib/pages/aboutus.dart
+++ lib/pages/aboutus.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/coustomIcon/toure_icons_icons.dart';
 
 class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'درباره ما',
-      home: Container(
-        child: Image.asset('about-1.jpg'),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('درباره ما'),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment.center,
+          textDirection: TextDirection.rtl,
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلام'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+                Text('ّسلامfgdfhdddfhfhfdhfhfrhfhfhfhfhfdhfhfhfgjhfgjfg'),
+              ],
+            ),
+            Positioned(
+                bottom: 25,
+                child: Row(
+                  children: <Widget>[
+                    InkWell(
+                      radius: 50,
+                      onTap: () {},
+                      child: Icon(
+                        ToureIcons.around,
+                        color: Colors.pink,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.map),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.mood),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.mail),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
