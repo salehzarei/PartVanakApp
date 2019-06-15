095c38df61e49ddb6de7fb516959c65a56d5d379
Salehz <saleh.zarei@gmail.com>
2019-06-15 18:46:04 +0430

    اضافه کردن اچ دی ام ال

--- lib/pages/aboutus.dart
+++ lib/pages/aboutus.dart
@@ -1,101 +1,103 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/coustomIcon/toure_icons_icons.dart';
-import 'package:hello_flutter/model/toure_model.dart';
+import 'package:flutter_html/flutter_html.dart';
+
 
 class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _text_long =
         'خرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجیخرید آنلاین بلیط هواپیما خارجی';
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'درباره ما',
-      home: Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Icon(
-                Icons.airplanemode_active,
-                color: Theme.of(context).accentColor,
-                size: 30,
-              ),
-              
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Icon(
+              Icons.airplanemode_active,
+              color: Theme.of(context).accentColor,
+              size: 30,
             ),
-            
-          ],
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          title: Text(
-            'درباره ما',
-            style: Theme.of(context).textTheme.title,
           ),
+        ],
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          'درباره ما',
+          style: Theme.of(context).textTheme.title,
         ),
-        body: Stack(
-          fit: StackFit.expand,
-          alignment: Alignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Container(
-                color: Colors.black26.withOpacity(0.2),
-                child: ListView(
-                  children: <Widget>[
-                    Text(
-                      '${_text_long.toString()}',
-                      textDirection: TextDirection.rtl,
-                      // maxLines: 5,
-                    ),
-                    //Spacer(),
-                    Text(
-                      '${_text_long.toString()}',
-                      textDirection: TextDirection.rtl,
-                      // maxLines: 100,
-                    ),
-                    Text(
-                      '${_text_long.toString()}',
-                      textDirection: TextDirection.rtl,
-                      // maxLines: 100,
-                    ),
-                  ],
-                ),
+      ),
+      body: Stack(
+        // fit: StackFit.expand,
+        alignment: Alignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              color: Colors.black26.withOpacity(0.2),
+              child: ListView(
+                children: <Widget>[
+                 Html(
+                   data: """
+                <div>Follow<a class='sup'><sup>pl</sup></a> 
+                  Below hr
+                    <b>Bold</b>
+                <h1>what was sent down to you from your Lord</h1>, 
+                and do not follow other guardians apart from Him. Little do</br>
+                 and do not follow other guardians apart from Him. Little do</br>
+                 and do not follow other guardians apart from Him. Little do</br>
+                 and do not follow other guardians apart from Him. Little do</br>
+                 and do not follow other guardians apart from Him. Little do</br>
+                 and do not follow other guardians apart from Him. Little do</br>
+                <span class='h'>you remind yourselves</span><a class='f'><sup f=2437>1</sup></a></div>
+                """,
+                 ),
+                
+                ],
               ),
             ),
-            Container(
-                child: Positioned(
-              bottom: 0,
-              child: Container(
-                color: Colors.black.withOpacity(1),
-                //alignment: Alignment.center,
-                //width: 500,
-                child: Row(
-                  children: <Widget>[
-                    InkWell(
-                        radius: 50,
-                        onTap: () {Navigator.pop(context);},
-                        child: Image.asset('images/instagram.png')),
-                    InkWell(
-                        radius: 50,
-                        onTap: () {},
-                        child: Image.asset('images/telegram.png')
-                        ),
-                    InkWell(
-                        radius: 50,
-                        onTap: () {},
-                        child: Image.asset('images/twitter.png')
-                        ),
-                    InkWell(
-                        radius: 20,
-                        onTap: () {},
-                        child: Image.asset('images/facebook.png')
-                        ),
-                  ],
-                ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+             // color: Colors.grey.withOpacity(1),
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(25),
+               color: Colors.grey.withOpacity(1),
+             ),
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset('images/instagram.png')),
+                     SizedBox(
+                       width: 10,
+                     ),
+                  InkWell(
+                      onTap: () {}, child: Image.asset('images/telegram.png')),
+                      SizedBox(
+                       width: 10,
+                     ),
+                  InkWell(
+                      onTap: () {}, child: Image.asset('images/twitter.png')),
+                       SizedBox(
+                       width: 10,
+                     ),
+                  InkWell(
+                      onTap: () {}, child: Image.asset('images/facebook.png')),
+                ],
               ),
-            ))
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
