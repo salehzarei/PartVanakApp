c1fea52e2d1aa5658fcbba5da87aef8284e81a2b
Salehz <saleh.zarei@gmail.com>
2019-06-17 20:33:27 +0430

    debug aboutus abbasi

--- lib/pages/aboutus.dart
+++ lib/pages/aboutus.dart
@@ -27,138 +27,142 @@ class _AboutUsState extends State<AboutUs> {
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
       builder: (BuildContext context, Widget child, MainModel model) {
-        return Directionality(
-          textDirection: TextDirection.rtl,
-          child: Stack(
-            //fit: StackFit.expand,
-            //textDirection: TextDirection.rtl,
-            children: <Widget>[
-              Container(
-                child: Image.network(
-                  'https://images.unsplash.com/photo-1500835556837-99ac94a94552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Scaffold(
-                  backgroundColor: Colors.transparent,
-                  drawer: MyDrawer(),
-                  appBar: AppBar(
-                    elevation: 0.0,
-                    backgroundColor: Colors.transparent,
-                    centerTitle: true,
-                    iconTheme: Theme.of(context).iconTheme,
-                    title: Text(
-                      'درباره ما',
-                      style: Theme.of(context).textTheme.title,
-                    ),
+        if (model.isLoading) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Stack(
+              //fit: StackFit.expand,
+              //textDirection: TextDirection.rtl,
+              children: <Widget>[
+                Container(
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1500835556837-99ac94a94552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                    fit: BoxFit.cover,
                   ),
-                  body: Stack(children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 80, bottom: 60, left: 15, right: 15),
-                      child: ListView(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: 10, right: 10, left: 10),
-                            child: RichText(
-                                textDirection: TextDirection.rtl,
-                                overflow: TextOverflow.fade,
-                                text: TextSpan(
-                                    text:
-                                        " ${model.aboutmodel.about} ${model.aboutmodel.about} ${model.aboutmodel.about}",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .body1
-                                        .copyWith(
-                                            color: Colors.white,
-                                            fontSize: 15))),
-                          ),
-
-                          Row(
-                            textDirection: TextDirection.rtl,
-                            children: <Widget>[
-                              Expanded(
-                                child: Text(
-                                  'نام متصدی :',
-                                  textDirection: TextDirection.rtl,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  '${model.aboutmodel.name}',
-                                  textDirection: TextDirection.rtl,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          // _buildCell(),
-                        ],
+                ),
+                Scaffold(
+                    backgroundColor: Colors.transparent,
+                    drawer: MyDrawer(),
+                    appBar: AppBar(
+                      elevation: 0.0,
+                      backgroundColor: Colors.transparent,
+                      centerTitle: true,
+                      iconTheme: Theme.of(context).iconTheme,
+                      title: Text(
+                        'درباره ما',
+                        style: Theme.of(context).textTheme.title,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: Container(
-                        // width: MediaQuery.of(context).size.width,
-                        height: 80,
-                        alignment: Alignment.center,
-                        color: Colors.transparent,
-                        child: Image.asset(
-                          'images/logo.png',
+                    body: Stack(children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 80, bottom: 60, left: 15, right: 15),
+                        child: ListView(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10, right: 10, left: 10),
+                              child: RichText(
+                                  textDirection: TextDirection.rtl,
+                                  overflow: TextOverflow.fade,
+                                  text: TextSpan(
+                                      text:
+                                          " ${model.aboutmodel.about} ${model.aboutmodel.about} ${model.aboutmodel.about}",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .body1
+                                          .copyWith(
+                                              color: Colors.white,
+                                              fontSize: 15))),
+                            ),
+
+                            Row(
+                              textDirection: TextDirection.rtl,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(
+                                    'نام متصدی :',
+                                    textDirection: TextDirection.rtl,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    '${model.aboutmodel.name}',
+                                    textDirection: TextDirection.rtl,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            // _buildCell(),
+                          ],
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 545, left: 15, right: 15),
-                      child: Material(
-                        color: Colors.transparent,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
                         child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              color: Theme.of(context)
-                                  .accentColor
-                                  .withOpacity(0.7)),
-                          height: 50,
-                          width: MediaQuery.of(context).size.width,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              InkWell(
-                                  onTap: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Image.asset('images/instagram.png')),
-                              SizedBox(
-                                width: 15,
-                              ),
-                              InkWell(
-                                  onTap: () {},
-                                  child: Image.asset('images/telegram.png')),
-                              SizedBox(
-                                width: 15,
-                              ),
-                              InkWell(
-                                  onTap: () {},
-                                  child: Image.asset('images/twitter.png')),
-                              SizedBox(
-                                width: 15,
-                              ),
-                              InkWell(
-                                  onTap: () {},
-                                  child: Image.asset('images/facebook.png')),
-                            ],
+                          // width: MediaQuery.of(context).size.width,
+                          height: 80,
+                          alignment: Alignment.center,
+                          color: Colors.transparent,
+                          child: Image.asset(
+                            'images/logo.png',
                           ),
                         ),
                       ),
-                    ),
-                  ])),
-            ],
-          ),
-        );
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 545, left: 15, right: 15),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Theme.of(context)
+                                    .accentColor
+                                    .withOpacity(0.7)),
+                            height: 50,
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                InkWell(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Image.asset('images/instagram.png')),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                InkWell(
+                                    onTap: () {},
+                                    child: Image.asset('images/telegram.png')),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                InkWell(
+                                    onTap: () {},
+                                    child: Image.asset('images/twitter.png')),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                InkWell(
+                                    onTap: () {},
+                                    child: Image.asset('images/facebook.png')),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ])),
+              ],
+            ),
+          );
+        }
       },
     );
   }
