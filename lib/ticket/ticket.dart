4c47f0314d1752a3d70a690b311ba797bf254efb
navidnejati <navidnejati76@gmail.com>
2019-08-28 19:22:47 +0430

    اتمام کار عباسی-نجاتی

--- lib/ticket/ticket.dart
+++ lib/ticket/ticket.dart
@@ -13,9 +13,9 @@ class TicketList extends StatefulWidget {
 }
 
 class _TicketListState extends State<TicketList> {
-  Ticket ticketlist;
   List<Ticket> list = List();
   MainModel model = new MainModel();
+
   // MainModel model;
   var isLoading = true;
 
@@ -42,15 +42,34 @@ class _TicketListState extends State<TicketList> {
       throw Exception('Failed to load ticket');
     }
   }
+///داریم نرم افزار را تست میکنیم
+  Widget _checkread(int read) {
+    if (read == 1) {
+      return Container(
+        child: Text(
+          'خوانده نشده',
+          style: TextStyle(color: Colors.redAccent),
+        ),
+      );
+    } else if (read == 2) {
+      return Container(
+          child: Text(
+        'پیام جدید',
+        style: TextStyle(color: Colors.greenAccent),
+      ));
+    } else {
+      return Container();
+    }
+  }
 
   @override
   void initState() {
     model.getToken().then((_) {
       if (model.userToken != null) {
         _getTicket();
-      }else{
+      } else {
         print('set false');
-         setState(() {
+        setState(() {
           isLoading = false;
         });
       }
@@ -64,7 +83,6 @@ class _TicketListState extends State<TicketList> {
       child: Text('موردی یافت نشد'),
     );
     if (model.userToken == null) {
-      
       return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -99,12 +117,28 @@ class _TicketListState extends State<TicketList> {
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: Text(
-                                ticket[index].subject,
-                                style: TextStyle(color: Colors.white),
-                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(20),
+                                  child: Text(
+                                    ticket[index].subject.length>30? ticket[index].subject.substring(0, 30)+'...' :ticket[index].subject,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 8, left: 20),
+                                  child: Container(
+                                      width: 80,
+                                      height: 20,
+                                      // color: Colors.white,
+                                      child: Container(
+                                        child: _checkread(ticket[index].read),
+                                      )),
+                                ),
+                              ],
                             ),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -127,9 +161,13 @@ class _TicketListState extends State<TicketList> {
                                     child: RaisedButton(
                                       child: Text('ادامه مطلب'),
                                       onPressed: () {
-                                        Navigator.push(context, MaterialPageRoute(builder: (content)=>Reply(ticket[index].id)));
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (content) =>
+                                                    Reply(ticket[index].id)));
                                         // Navigator.pushNamed(
-                                            // context, '/newreply');
+                                        // context, '/newreply');
                                       },
                                     ),
                                   ),
@@ -167,71 +205,94 @@ class _TicketListState extends State<TicketList> {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Scaffold(
-          drawer: MyDrawer(),
-          appBar: AppBar(
-            iconTheme: Theme.of(context)
-                .iconTheme
-                .copyWith(color: Theme.of(context).accentColor),
-            centerTitle: true,
-            title: Text('تیکت ها', style: Theme.of(context).textTheme.display2),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.arrow_forward,
-                    color: Theme.of(context).accentColor),
-                onPressed: () => Navigator.pushNamed(context, '/homepage'),
-              )
-            ],
-          ),
-          body: isLoading
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Stack(
-                  children: <Widget>[
-                    makeTicketList(list),
+      child: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Scaffold(
+              drawer: MyDrawer(),
+              appBar: AppBar(
+                iconTheme: Theme.of(context)
+                    .iconTheme
+                    .copyWith(color: Theme.of(context).accentColor),
+                centerTitle: true,
+                title: Text('تیکت ها',
+                    style: Theme.of(context).textTheme.display2),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward,
+                        color: Theme.of(context).accentColor),
+                    onPressed: () => Navigator.pushNamed(context, '/homepage'),
+                  )
+                ],
+              ),
+              body: Stack(
+                children: <Widget>[
+                  makeTicketList(list),
 
-                    /////////////////////////پیام جدید////////////////////////////
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                            //decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                            height: 50,
-                            width: MediaQuery.of(context).size.width,
-                            color: Colors.grey.shade800,
-                            child: Column(
-                              children: <Widget>[
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(context, '/addticket');
-                                  },
-                                  child: Container(
-                                    width: 125,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.message,
-                                          color: Theme.of(context).accentColor,
-                                        ),
-                                        Text(
-                                          'پیام جدید',
-                                          style:
-                                              DarwerThemeData.textTheme.title,
-                                        )
-                                      ],
-                                    ),
+                  /////////////////////////پیام جدید////////////////////////////
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top:5),
+                          //decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.grey.shade800,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              // SizedBox(
+                              //   height: 5,
+                              // ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushReplacementNamed(context, '/addticket');
+                                },
+                                child: Container(
+                                  width: 125,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.message,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      Text(
+                                        'پیام جدید',
+                                        style: DarwerThemeData.textTheme.title,
+                                      )
+                                    ],
                                   ),
                                 ),
-                              ],
-                            )),
-                      ],
-                    ),
-                  ],
-                )),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/ticket');
+                                },
+                                child: Container(
+                                  width: 125,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.refresh,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      Text(
+                                        'بارگزاری',
+                                        style: DarwerThemeData.textTheme.title,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
+                ],
+              )),
     );
   }
 }
